00482b156aec6ef0c149e1cbdc654e2c3e82d75b
dt <dtvangogh@github.com>
2020-05-16 00:32:50 -0400

    sweep.py

--- sweep.py
+++ sweep.py
@@ -205,6 +205,7 @@ class InstaBot:
                     pass
             print("{:d} main.c files created".format(
                 number_of_project_files))
+            print("README.md created")
             print('\n')
             print("GOOD LUCK WITH YOUR PROJECT")
             print('\n')
@@ -324,7 +325,6 @@ class InstaBot:
                 '/html/body/main/article/h1').text
         except NoSuchElementException:
             pass
-        print(project_title)
 
         question_title_array = []
 
@@ -349,7 +349,18 @@ class InstaBot:
             except NoSuchElementException:
                 pass
         question_title_array = list(dict.fromkeys(question_title_array))
-        print(question_title_array)
+        question_title_array_without_mandatory = []
+        substring = 'mandatory'
+        substring2 = '#advanced'
+        for question_title_array in question_title_array:
+            if question_title_array.endswith(substring):
+                remove_mandatory = question_title_array[:-(len(substring))]
+                question_title_array_without_mandatory.append(remove_mandatory)
+            elif question_title_array.endswith(substring2):
+                remove_advanced = question_title_array[:-(len(substring2))]
+                question_title_array_without_mandatory.append(
+                    remove_advanced)
+
         question_description_array = []
         for i in range(0, number_of_project_files + 1):
             try:
@@ -360,34 +371,114 @@ class InstaBot:
                 pass
         f = open('README.md', "w+")
         f.write(
-            "# {}\n## Description\n This project is part of {}:\n## Project tasks :wrench:\n\n### [{}](./{}) \n* {}\n".format(project_title, readme_repo_name, question_title_array[0], readme_file_name_array[0], question_description_array[0]))
+            "# {}\n## Description\n This project is part of {}:\n## Project tasks :wrench:\n".format(project_title, readme_repo_name))
         f = open('README.md', "a")
-        f.write(
-            "### [{}](./{}) \n* {}\n".format(question_title_array[1], readme_file_name_array[1], question_description_array[1]).format(i))
-        f = open('README.md', "a")
-        f.write(
-            "### [{}](./{}) \n* {}\n".format(question_title_array[2], readme_file_name_array[2], question_description_array[2]).format(i))
-        f = open('README.md', "a")
-        f.write(
-            "### [{}](./{}) \n* {}\n".format(question_title_array[3], readme_file_name_array[3], question_description_array[3]).format(i))
-        f = open('README.md', "a")
-        f.write(
-            "### [{}](./{}) \n* {}\n".format(question_title_array[4], readme_file_name_array[4], question_description_array[4]).format(i))
-        f = open('README.md', "a")
+# CREATE README
+        question_title_array = question_title_array_without_mandatory
+        print(question_title_array)
         try:
             f.write(
-                "### [{}](./{}) \n* {}\n".format(question_title_array[55], readme_file_name_array[55], question_description_array[55]).format(i))
+                "### [{}](./{}) \n* {}\n".format(question_title_array[0], readme_file_name_array[0], question_description_array[0]))
         except IndexError:
             pass
-
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[1], readme_file_name_array[1], question_description_array[1]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[2], readme_file_name_array[2], question_description_array[2]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[3], readme_file_name_array[3], question_description_array[3]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[4], readme_file_name_array[4], question_description_array[4]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[5], readme_file_name_array[5], question_description_array[5]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[6], readme_file_name_array[6], question_description_array[6]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[7], readme_file_name_array[7], question_description_array[7]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[8], readme_file_name_array[8], question_description_array[8]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[9], readme_file_name_array[9], question_description_array[9]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[10], readme_file_name_array[10], question_description_array[10]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[11], readme_file_name_array[11], question_description_array[11]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[12], readme_file_name_array[12], question_description_array[12]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[13], readme_file_name_array[13], question_description_array[13]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[14], readme_file_name_array[14], question_description_array[14]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[15], readme_file_name_array[15], question_description_array[15]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[16], readme_file_name_array[16], question_description_array[16]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[17], readme_file_name_array[17], question_description_array[17]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[18], readme_file_name_array[18], question_description_array[18]))
+        except IndexError:
+            pass
+        try:
+            f.write(
+                "### [{}](./{}) \n* {}\n".format(question_title_array[19], readme_file_name_array[19], question_description_array[19]))
+        except IndexError:
+            pass
         f = open('README.md', "a")
         f.write(
             '---\nCreated by [DT Van](https://github.com/dtvangogh)')
-        print("README.md made")
-        print(question_description_array)
-        print(question_title_array[0])
-        print(readme_file_name_array[0])
-        print(question_description_array[0])
 
 
 InstaBot('1661@holbertonschool.com', password)
